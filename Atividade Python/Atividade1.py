29e69db8f7fcf570b63b517a52bd490812c14ca0
wesleyslhessaren-collab <wesley_slhessaren@estudante.sesisenai.org.br>
2026-02-18 20:23:06 +0000

    update

---
+++ Atividade Python/Atividade1.py
@@ -0,0 +1,8 @@
+a = "ola"
+b = "mundo"
+c = a+b
+print(c)
+d = 100
+e = 50
+f = d+e
+print(f)
---
+++ Atividade Python/atividade2.py
@@ -0,0 +1,7 @@
+a = input("insira um numero")
+b = input("insira o segundo numero")
+print (a+b)
+
+a = float(input("insira um numero"))
+b = float(input("insira o segundo numero"))
+print(a+b)
