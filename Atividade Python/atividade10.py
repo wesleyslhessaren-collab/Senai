2da5e622d890c5f3355985f7502382fea1db57d2
wesleyslhessaren-collab <wesley_slhessaren@estudante.sesisenai.org.br>
2026-03-11 19:22:10 +0000

    update

--- Atividade Python/atividade10.py
+++ Atividade Python/atividade10.py
@@ -1,27 +1,25 @@
-medicoes = []
+soma = 0
+acima_15 = 0
+acima_20 = 0
+acima_200 = 0
+sobrecarga = False
+alerta = False
+for i in range (8):
+    corrente = float(input(f"Digite a {i+1} medição de corrente (A): "))
+    soma += corrente
 
-print("Digite 8 medições de corrente elétrica (em Amperes):")
-for i in range(8):
-    corrente = float(input(f"Digite a medição {i+1} de corrente (A): "))
-    medicoes.append(corrente)
-
-
-acima_15a = sum(1 for m in medicoes if m > 15)
-
-
-sobrecarga = any(m > 20 for m in medicoes)
-
-
-media_corrente = sum(medicoes) / len(medicoes)
-
-alerta_200 = any(m > 200 for m in medicoes)
-
-
-print(f"Medições acima de 15A: {acima_15a}")
-print(f"Há sobrecarga (acima de 20A): {'SIM' if sobrecarga else 'NÃO'}")
-print(f"Média da corrente: {media_corrente:.2f}A")
-
-if alerta_200:
-    print(" ALERTA: Uma ou mais medições ultrapassaram 200A!")
-else:
-    print("Todas as medições estão dentro dos limites de segurança.")
+    if corrente > 15:
+        acima_15 += 1 
+    if corrente > 20:
+        sobrecarga = True
+        acima_20 += 1
+    if corrente > 200:
+        alerta = True
+        acima_200 += 1
+media = soma / 8
+print(f"mediçoes acima 15 (A)", acima_15)
+print("media da corrente:", media)
+if sobrecarga:
+    print (f"houve Sobrecarga no motor {acima_20} vezes")
+if alerta:
+    print(f"alerta: medição ultrapassou 200 {acima_200} vezes")   
